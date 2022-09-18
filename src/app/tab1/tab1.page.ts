0ded20691dc6081d8744b318f0f3b234616bc68f
Leonardo Ivo Folchini <leonardo.folchini@hotmail.com>
2022-09-17 22:29:42 -0300

    Adicionado o array e a interface dos filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,27 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Filmes';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Top Gun: Maverick (2022)',
+      lancamento: '26/05/2022',
+      duracao: '2h 11m',
+      classificacao: 84,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jTrXwK56EoLHHxfBkpwGdfmy2uh.jpg',
+      generos: ['Ação', 'Drama']
+    },
+    {
+      nome: 'Sorte (2022)',
+      lancamento: '05/08/2022',
+      duracao: '1h 37m',
+      classificacao: 80,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aeJHkkrDvbXPB25NghNbv5kUBVi.jpg',
+      generos: ['Animação', 'Aventura', 'Comédia', 'Fantasia']
+    }
+  ];
 
   constructor(private alertController: AlertController, private toastController: ToastController) { }
 
